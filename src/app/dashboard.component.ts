45b718eff1f97591b316b05cfbc9b9d7a8ccfdf5
Codenator81 <Donchenko34>
2016-12-23 21:18:01 +0200

    integrate material

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -17,6 +17,6 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.heroService.visibleHeroes$
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+      .subscribe(heroes => this.heroes = heroes.slice(0, 4));
   }
 }

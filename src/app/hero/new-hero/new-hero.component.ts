6d037ef181246c6d97f24e857aded0a7d90408d5
Codenator81 <Donchenko34>
2016-12-29 19:45:35 +0200

    same more animations

--- src/app/hero/new-hero/new-hero.component.ts
+++ src/app/hero/new-hero/new-hero.component.ts
@@ -1,10 +1,32 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+  Component, EventEmitter, Input, Output, style, animate, transition, state, trigger,
+  keyframes
+} from '@angular/core';
 import {Hero} from "../../models/hero";
 
 @Component({
   selector: 'app-new-hero',
   templateUrl: './new-hero.component.html',
-  styleUrls: ['./new-hero.component.scss']
+  styleUrls: ['./new-hero.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        animate(300, keyframes([
+          style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+          style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
+          style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+        ]))
+      ]),
+      transition('* => void', [
+        animate(300, keyframes([
+          style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+          style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
+          style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0})
+        ]))
+      ])
+    ])
+  ]
 })
 export class NewHeroComponent {
 
@@ -12,9 +34,21 @@ export class NewHeroComponent {
   @Input() powers: Array<string>;
   @Output() saveRequest = new EventEmitter<Hero>();
 
-  onSubmit(isValid) {
+  showHero: Boolean = false;
+  flyState = 'in';
+
+    onSubmit(isValid) {
     if (isValid) {
       this.saveRequest.emit(this.hero);
     }
+  }
+
+  switchState() {
+    if (this.showHero) {
+      this.flyState = 'in';
+    } else {
+      this.flyState = 'out';
+    }
+    console.log(this.flyState);
   }
 }
--- src/app/shared/directives/highlight.directive.ts
+++ src/app/shared/directives/highlight.directive.ts
@@ -1,13 +1,25 @@
-import { Directive, ElementRef } from '@angular/core';
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 
 @Directive({ selector: '[highlight]' })
 /** Highlight the attached element in gold */
 export class HighlightDirective {
-  constructor(el: ElementRef) {
-    el.nativeElement.style.backgroundColor = 'gold';
-    el.nativeElement.style.color = '#000';
+  constructor(private el: ElementRef) {
+    el.nativeElement.style.color = '#fff';
     el.nativeElement.style.padding = '20px';
     console.log(
       `* AppRoot highlight called for ${el.nativeElement.tagName}`);
+  }
+  @Input() highlightColor: string;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.highlightColor || 'red');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
   }
 }
